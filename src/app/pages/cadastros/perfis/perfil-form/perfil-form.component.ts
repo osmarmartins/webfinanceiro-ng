5c8d08051e0aa90404df140ea0c9a1cf5b24a246
osmar-dell <osmarfilh6@gmail.com>
2022-10-15 20:38:05 -0300

    release/atualizacaoDependencias

--- src/app/pages/cadastros/perfis/perfil-form/perfil-form.component.ts
+++ src/app/pages/cadastros/perfis/perfil-form/perfil-form.component.ts
@@ -1,12 +1,11 @@
-import { NgxUiLoaderService } from 'ngx-ui-loader';
-import { AutorizacaoService } from './../../../../core/services/autorizacao.service';
 import { Component, ElementRef, OnInit, ViewChild, AfterViewInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
+import { NgxUiLoaderService } from 'ngx-ui-loader';
 import { Autorizacao } from 'src/app/core/models/autorizacao';
 import { Perfil } from 'src/app/core/models/perfil';
 import { PerfilService } from 'src/app/core/services/perfil.service';
 import { AppToastService } from 'src/app/core/services/toast.service';
-import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
+import { AutorizacaoService } from 'src/app/core/services/autorizacao.service';
 
 @Component({
   selector: 'app-perfil-form',
